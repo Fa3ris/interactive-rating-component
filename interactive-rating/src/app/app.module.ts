be77528e02d8f3e7cbcc1de035aaa8261c7f4d37
FabriceT <fabrice.tsiava@artik-consulting.com>
2022-03-31 16:00:22 +0200

    init rating component

--- interactive-rating/src/app/app.module.ts
+++ interactive-rating/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RatingCardComponent } from './rating-card/rating-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RatingCardComponent
   ],
   imports: [
     BrowserModule
