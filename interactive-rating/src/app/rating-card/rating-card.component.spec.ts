02155195695e64604893d62c2dcab2fee110074c
FabriceT <fabrice.tsiava@artik-consulting.com>
2022-03-31 21:44:16 +0200

    test click on rating

--- interactive-rating/src/app/rating-card/rating-card.component.spec.ts
+++ interactive-rating/src/app/rating-card/rating-card.component.spec.ts
@@ -1,10 +1,13 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { RatingCardComponent } from './rating-card.component';
 
 describe('RatingCardComponent', () => {
   let component: RatingCardComponent;
   let fixture: ComponentFixture<RatingCardComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +19,38 @@ describe('RatingCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RatingCardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    debugElement = fixture.debugElement
+    fixture.detectChanges(); // trigger change detection
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+
+   
   });
+
+  it('selects the clicked rating', () => {
+
+    const rating2 = (debugElement.query(By.css('[data-testid="2"]')));
+
+    const nativeElt = rating2.nativeElement as HTMLElement
+
+    console.log("before click", nativeElt.classList)
+
+    // WARNING: event does not bubble
+    rating2.triggerEventHandler(
+        'click', 
+        null // do not care about Event properties
+    )
+
+    console.log("after click but no change detection", nativeElt.classList)
+
+    fixture.detectChanges();
+
+    console.log("after click after change detection", nativeElt.classList)
+    
+    expect(nativeElt.classList.contains("selected")).toBeTrue();
+
+  })
 });
